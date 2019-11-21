381ca5a0322462a5f539a43384ccea63def05f8f
d0ku <darthtyranus666666@gmail.com>
2019-11-21 21:57:24 +0100

    added image diffs

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,46 @@
 FROM python:3.7
 
+# Install chromium (version 78) and matching chromedriver.
+ARG CHROMIUM_REVISION=693954
+WORKDIR /root
+RUN wget -O chrome-linux.zip https://storage.googleapis.com/chromium-browser-snapshots/Linux_x64/$CHROMIUM_REVISION/chrome-linux.zip
+RUN pwd && ls && unzip chrome-linux.zip && mv chrome-linux/* /usr/bin
+
+RUN wget -O chromedriver.zip http://chromedriver.storage.googleapis.com/78.0.3904.70/chromedriver_linux64.zip
+RUN unzip chromedriver.zip
+RUN mv chromedriver /usr/bin/chromedriver && \
+    chown root:root /usr/bin/chromedriver && \
+    chmod +x /usr/bin/chromedriver
+
+RUN apt-get update && \
+    apt-get install -y curl \
+    unzip \
+    xvfb \
+    libxi6 \
+    libgconf-2-4 \
+    libxcomposite-dev \
+    libxcursor-dev \
+    libxtst-dev \
+    libnss3-dev \
+    libcups2 \
+    libxrandr-dev \
+    libasound-dev \
+    libatk1.0-0 \
+    libatk-bridge2.0-0 \
+    libgtk-3-dev
+
+# Set up app.
 WORKDIR /app
 COPY ./requirements.txt .
 RUN pip3 install -r requirements.txt
-COPY . .
 
+ENV PYTHONUNBUFFERED=1
 ENV SLEEP_TIME=600
-
 #ENV URL_TO_CHECK=url \
 #    MAIL_RECIPIENT=recipient \
 #    MAIL_SENDER=sender \
 #    SENDGRID_API_KEY=key
+
+COPY . .
 
 ENTRYPOINT ["python3", "monitor.py"]
--- monitor.py
+++ monitor.py
@@ -1,8 +1,9 @@
 """Page Monitor
 Periodically check for changes in HTML code of webpage and notify users if
-detected.
+detected (also send diffs).
 d0ku 2019"""
 
+import base64
 import requests
 import os
 import sys
@@ -10,7 +11,43 @@ import logging
 import time
 
 from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+from sendgrid.helpers.mail import (
+    Mail, Attachment, FileContent, FileName, FileType, Disposition, ContentId )
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from PIL import Image
+from PIL import ImageChops
+from PIL import ImageDraw
+from PIL import ImageColor
+
+def screenshot_url(url, filename):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--start-maximized")
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(url)
+    time.sleep(3)
+
+    # Just some big value.
+    driver.set_window_size(1920, 2160)
+
+    screenshot = driver.save_screenshot(filename)
+    driver.quit()
+
+def mark_diff_images(first, second, output):
+    im1 = Image.open(first)
+    im2 = Image.open(second)
+
+    im_diff = ImageChops.difference(im1, im2)
+    diff = im_diff.getbbox()
+    im2.crop(diff).save('real_diff.png')
+    draw = ImageDraw.Draw(im2)
+    diff_list = list(diff) if diff else []
+    draw.rectangle(diff_list, outline=ImageColor.getrgb("red"), width=5)
+    im2.convert('RGB').save(output)
 
 def check_for_variable_existence(variables):
     logger = logging.getLogger('checker')
@@ -22,10 +59,11 @@ def check_for_variable_existence(variables):
 
 
 def started_mail(url):
+    return
     logger = logging.getLogger('email')
     if not check_for_variable_existence(['MAIL_RECIPIENT', 'MAIL_SENDER',
                                          'SENDGRID_API_KEY']):
-        return
+        sys.exit(1)
 
     message = Mail(
         from_email=os.environ.get('MAIL_SENDER'),
@@ -35,7 +73,9 @@ def started_mail(url):
 <br>
 It will be checked every {1} seconds. We will let you know if something changes.
 <br>
-MAY THE FORCE BE WITH YOU""".format(url, os.environ.get("SLEEP_TIME"))
+MAY THE FORCE BE WITH YOU
+<br>
+We are changing for the better :)""".format(url, os.environ.get("SLEEP_TIME"))
     )
 
     try:
@@ -49,19 +89,37 @@ MAY THE FORCE BE WITH YOU""".format(url, os.environ.get("SLEEP_TIME"))
 
 
 def changes_mail(url):
+    mark_diff_images("old_state.png", "new_state.png", "diff.png")
+    print("CHANGED")
     logger = logging.getLogger('email')
     if not check_for_variable_existence(['MAIL_RECIPIENT', 'MAIL_SENDER',
                                          'SENDGRID_API_KEY']):
-        return
+        sys.exit(1)
 
     message = Mail(
         from_email=os.environ.get('MAIL_SENDER'),
         to_emails=os.environ.get("MAIL_RECIPIENT"),
         subject="Changes detected on " + url,
         html_content="""Changes were detected on the page you ordered to monitor.
-        <br>
+<br>
+Take a look at included attachments to see them. (They are marked with red.)
 MAY THE FORCE BE WITH YOU"""
     )
+
+    def attach_png(file_path):
+        with open(file_path, 'rb') as f:
+                data = f.read()
+                f.close()
+        encoded = base64.b64encode(data).decode()
+        attachment = Attachment()
+        attachment.file_content = FileContent(encoded)
+        attachment.file_type = FileType('image/png')
+        attachment.file_name = FileName(file_path)
+        attachment.disposition = Disposition('attachment')
+        attachment.content_id = ContentId(file_path)
+        return attachment
+
+    message.attachment = [attach_png('diff.png'), attach_png('real_diff.png')]
 
     try:
         sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
@@ -78,7 +136,6 @@ def main():
     if not check_for_variable_existence(['URL_TO_CHECK', 'SLEEP_TIME']):
         sys.exit(1)
 
-    started_mail(os.environ.get('URL_TO_CHECK'))
     try:
         int(os.environ.get("SLEEP_TIME"))
     except ValueError:
@@ -86,9 +143,13 @@ def main():
         sys.exit(3)
 
     text = None
+    url = os.environ.get('URL_TO_CHECK')
+    screenshot_url(url, "old_state.png")
+    started_mail(url)
+    print("STARTED")
     while True:
         logger.debug("Loop")
-        temp = requests.get(os.environ.get("URL_TO_CHECK"))
+        temp = requests.get(url)
         if text is None:
             # First time check.
             text = temp.text
@@ -98,10 +159,13 @@ def main():
             if temp.text == text:
                 logger.info("Nothing changed on website.")
             else:
+                print("CHANGES START")
                 logger.info("Changes detected.")
-                changes_mail(os.environ.get("URL_TO_CHECK"))
+                screenshot_url(url, "new_state.png")
+                changes_mail(url)
+                os.rename("new_state.png", "old_state.png")
                 text = temp.text
+                print("CHANGES DONE")
         time.sleep(int(os.environ.get("SLEEP_TIME")))
-
 
 main()
