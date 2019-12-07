031e71b0fde66c2aecaf02202bea77ecd721b835
d0ku <darthtyranus666666@gmail.com>
2019-12-07 22:28:59 +0100

    authorization should work fine, dummy endpoint and protectedRoute

--- front/src/Login.tsx
+++ front/src/Login.tsx
@@ -7,15 +7,51 @@ import {
   FormLabel,
   FormControl,
   Button,
-  Card
+  Card,
+  Modal
 } from "react-bootstrap";
+
+import axios from "axios";
+import ClipLoader from 'react-spinners/ClipLoader';
 
 const Login: React.FC = () => {
   const [email, setEmail] = React.useState("");
+  const [showModal, setShowModal] = React.useState(false);
+  const [modalContent, setModalContent] = React.useState("");
+  const [successfullSent, setSuccessfullSent] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
 
   function handleSubmit(e: any) {
-    e.preventDefault();
-    console.log("SUBMIT!");
+      e.preventDefault();
+      let config = {timeout: 10000}
+      let data = {
+	  email: email,
+	  redirectUri: window.location.protocol + "//" + window.location.hostname + "/login/parser/"
+      }
+
+      setLoading(true);
+      axios.post(process.env.REACT_APP_API_SERVER + "/passwordless/start", data, config)
+	  .then((response) => {
+	      if (response.status !== 204) {
+		  // Something went wrong on server side.
+		  console.log(response);
+		  if (response.status === 503) {
+		      setModalContent("There was a problem with mail service. Please contact an admin about that.");
+		  } else {
+		      setModalContent("Something went wrong... Please try again later.");
+		  }
+		  setShowModal(true);
+	      } else {
+		  // Everything is good.
+		  setSuccessfullSent(true);
+	      }
+	  }).catch((error) => {
+	    // Something went wrong with sending.
+	      console.log(error);
+	      setModalContent("Something went wrong... Please try again later.");
+	      setShowModal(true);
+	  });
+      setLoading(false);
   }
 
   function validateForm() {
@@ -25,8 +61,14 @@ const Login: React.FC = () => {
   return (
     <div className="container-fluid">
       <div className="Login">
+	<Modal show={showModal} onHide={() => {setShowModal(false);}}>
+	  <Modal.Header closeButton>
+	    <Modal.Title>Success</Modal.Title>
+	  </Modal.Header>
+	  <Modal.Body>{modalContent}</Modal.Body>
+	</Modal>
         <Card bg="light" id="centered-form">
-          <Card.Body>
+          <Card.Body hidden={successfullSent || loading}>
             <form onSubmit={handleSubmit}>
               <FormGroup controlId="email">
                 <FormLabel>Email</FormLabel>
@@ -42,10 +84,40 @@ const Login: React.FC = () => {
               </Button>
             </form>
           </Card.Body>
+	  <div>
+	  <Card.Body hidden={!successfullSent || loading}>
+	    <Card.Title>Good job!</Card.Title>
+	    If your mail is in our database, you will shortly get a message with an one-time authenticating link. Click it, and you will be logged in.
+	  </Card.Body>
+	  <Card.Body hidden={!loading}>
+	    <ClipLoader
+	      size={150}
+	      />
+	  </Card.Body>
+	  </div>
         </Card>
       </div>
     </div>
   );
 };
+
+const token_session_name = "auth_token";
+
+export function getToken() {
+        // TODO: REMOVE IN PRODUCTION.
+        if (process.env.NODE_ENV === "development") {
+            window.sessionStorage.setItem(token_session_name, "DEV_TOKEN");
+        }
+       console.log(window.sessionStorage.getItem(token_session_name));
+       return window.sessionStorage.getItem(token_session_name);
+}
+
+export function setToken(token : string) {
+       window.sessionStorage.setItem(token_session_name, token);
+}
+
+export function logOut() {
+       window.sessionStorage.setItem(token_session_name, "");
+}
 
 export default Login;
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,10 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import './index.css';
-import Login from './Login';
+import Login, {getToken, setToken} from './Login';
 import * as serviceWorker from './serviceWorker';
+import queryString from "query-string";
 
-ReactDOM.render(<Login />, document.getElementById('root'));
+ReactDOM.render((
+    <Router>
+      <Switch>
+	    <Route path="/login/parser" component={LoginParser}>
+	    </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+	    <PrivateRoute path="/">
+	      <Home />
+	    </PrivateRoute>
+        </Switch>
+    </Router>), document.getElementById('root'));
+
+function LoginParser(props : any) {
+    let responseData = queryString.parse(props.location.hash);
+    if (typeof responseData.jwt === "string") {
+       setToken(responseData.jwt);
+    }
+    return <Redirect to="/" />;
+}
+
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function PrivateRoute({ children, ...rest } : any) {
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        getToken() ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
