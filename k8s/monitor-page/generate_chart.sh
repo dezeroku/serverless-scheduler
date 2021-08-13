85740edbc3927680b7b78093a6551671c7e3851d
d0ku <darthtyranus666666@gmail.com>
2021-08-13 23:32:29 +0200

    [k8s][CI] Set proper hash to be used for PR generated Helm chart
    Should be enough to close #13

--- k8s/monitor-page/generate_chart.sh
+++ k8s/monitor-page/generate_chart.sh
@@ -5,11 +5,11 @@
 
 set -e
 
-if [ -z "${GIT_HASH}" ]; then
+if [ -z "${MONITOR_CHART_GIT_HASH}" ]; then
     echo "Trying to read git hash from the repo"
-    GIT_HASH="$(git rev-parse HEAD)"
+    MONITOR_CHART_GIT_HASH="$(git rev-parse HEAD)"
 fi
 
-echo "The GIT_HASH to be used is: ${GIT_HASH}"
+echo "The MONITOR_CHART_GIT_HASH to be used is: ${MONITOR_CHART_GIT_HASH}"
 
-sed "s/%%SHA-SPECIAL-TOKEN%%/${GIT_HASH}/" Chart.tpl > Chart.yaml
+sed "s/%%SHA-SPECIAL-TOKEN%%/${MONITOR_CHART_GIT_HASH}/" Chart.tpl > Chart.yaml
