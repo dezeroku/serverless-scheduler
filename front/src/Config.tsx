6b8344ca4957694c32b55b2ad152c3dd7183698a
dezeroku <dezeroku@gmail.com>
2022-02-09 23:42:58 +0100

    [front] Try to get the API URL based on the current hostname

--- front/src/Config.tsx
+++ front/src/Config.tsx
@@ -2,8 +2,24 @@
 
 declare global {
     interface Window {
-        _env_: { REACT_APP_API_URL: string };
+        _env_: {
+            REACT_APP_API_URL: string,
+            USE_ENV_REACT_APP_API_URL: string
+        };
     }
 }
 
-export const API_URL: string = process.env.NODE_ENV === "development" ? process.env.REACT_APP_API_URL as string : window._env_.REACT_APP_API_URL;
+
+function getAPIURL() : string {
+    if (process.env.NODE_ENV === "development") {
+        return process.env.REACT_APP_API_URL as string;
+    } else {
+        if (window._env_.USE_ENV_REACT_APP_API_URL === 'true') {
+            return window._env_.REACT_APP_API_URL;
+        }
+    }
+    // Try to automagically generate it based on the domain
+    return 'api.' + window.location.hostname;
+}
+
+export const API_URL: string = getAPIURL();
