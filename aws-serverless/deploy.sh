ab594ce5f0e39f2b8a5c116f5121135145437004
dezeroku <dezeroku@gmail.com>
2022-02-14 02:44:22 +0100

    Add domain logic in deploy.sh

--- aws-serverless/deploy.sh
+++ aws-serverless/deploy.sh
@@ -7,6 +7,13 @@ pushd ../front
 make build
 popd
 
+# Configure domain (the env variable is needed)
+[ -z "${apiDomain}" ] && apiDomain=example.com
+export apiDomain
+sls create_domain
+
 # Deploy files
 serverless client deploy --no-confirm
+
+# Deploy an actual app
 serverless deploy
