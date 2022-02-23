3e08ca6b600104d10f74dfe8f6964b3f01d91088
dezeroku <dezeroku@gmail.com>
2022-02-23 23:49:09 +0100

    Use more decorators in the items getter

--- aws-serverless/items/items.py
+++ aws-serverless/items/items.py
@@ -2,37 +2,39 @@ import json
 import logging
 import os
 
-from lambda_decorators import cors_headers
+from lambda_decorators import cors_headers, json_http_resp
 import boto3
 dynamodb = boto3.resource('dynamodb')
 
 from common import cognito
 
+# To be used later on with schemas
+from common.schemas import itemwithid_schema
+
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 @cors_headers
+@json_http_resp
 def get(event, context):
     table = dynamodb.Table(os.environ['DYNAMO_DB'])
 
     user = cognito.get_username(event)
 
-
     # Actually getting the data from a row:
-
     result = table.get_item(
         Key={
             'user_id': user
         }
     )
 
-    if 'Item' not in result:
-        # TODO: This is a big hack
-        # such a logic should be handled on the Lambda level
-        # This Lambda should listed to signup events of the cognito pool
-        # and create rows as needed accordingly
-        # Row creation if needed
 
+    # TODO: This is a big hack
+    # such a logic should be handled on the Lambda level
+    # This Lambda should listed to signup events of the cognito pool
+    # and create rows as needed accordingly
+    # Row creation if needed
+    if 'Item' not in result:
         logger.info(f'HACK: putting the initial data for {user} in the table')
         response = table.put_item(
         Item={
@@ -48,9 +50,4 @@ def get(event, context):
             }
         )
 
-    logger.info(result)
-
-    status_code = 200
-    content = result['Item']['monitors']
-
-    return {'statusCode': status_code, 'body': json.dumps(content)}
+    return result['Item']['monitors']
