b8ff71c58a041018113f902034c2baa3f4f7186c
dezeroku <dezeroku@gmail.com>
2022-02-23 21:25:19 +0100

    WIP

--- aws-serverless/items/items.py
+++ aws-serverless/items/items.py
@@ -2,19 +2,30 @@ import json
 import logging
 import os
 
-from common import cognito
-
 from lambda_decorators import cors_headers
+import boto3
+dynamodb = boto3.resource('dynamodb')
+
+from common import cognito
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 @cors_headers
 def get(event, context):
-    status_code = 200
-    content = []
+    table = dynamodb.Table(os.environ['DYNAMO_DB'])
 
-    logger.info(cognito.get_username(event))
-    # Return the data from DynamoDB
+    user = cognito.get_username(event)
+
+    result = table.get_item(
+        Key={
+            'user_id': user
+        }
+    )
+
+    logger.info(result)
+
+    status_code = 200
+    content = result['Item']['monitors']
 
     return {'statusCode': status_code, 'body': json.dumps(content)}
