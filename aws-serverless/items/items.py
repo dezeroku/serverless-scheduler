79ce66d694eac65899b97b2bc7ce5cf058cb1ef1
dezeroku <dezeroku@gmail.com>
2022-02-23 22:06:46 +0100

    dirty hack to create the initial data structure in dynamodb

--- aws-serverless/items/items.py
+++ aws-serverless/items/items.py
@@ -17,11 +17,36 @@ def get(event, context):
 
     user = cognito.get_username(event)
 
+
+    # Actually getting the data from a row:
+
     result = table.get_item(
         Key={
             'user_id': user
         }
     )
+
+    if 'Item' not in result:
+        # TODO: This is a big hack
+        # such a logic should be handled on the Lambda level
+        # This Lambda should listed to signup events of the cognito pool
+        # and create rows as needed accordingly
+        # Row creation if needed
+
+        logger.info(f'HACK: putting the initial data for {user} in the table')
+        response = table.put_item(
+        Item={
+                'user_id': user,
+                'monitors': []
+            }
+        )
+
+        # Doing it in such an ugly way, to make sure that the data is in place
+        result = table.get_item(
+            Key={
+                'user_id': user
+            }
+        )
 
     logger.info(result)
 
