34089f780842fcf24de5c2ae830aad816804924a
dezeroku <dezeroku@gmail.com>
2022-02-23 22:54:50 +0100

    Init for schemas

---
+++ aws-serverless/create_schemas.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+function create_schemas() {
+    # Generate the .jsons
+    pushd common
+    openapi2jsonschema ../swagger/swagger.yaml --stand-alone
+
+    # Make the .jsons part of .py
+    rm -f schemas.py
+
+    pushd schemas
+
+    for x in $(find . -name "*.json"); do
+        var_name="${x%%.json}"
+        var_name="${var_name##./}_schema"
+        out_name="${x%%.json}.py"
+        ../../json2py.py $x $out_name $var_name
+    done
+
+    popd
+
+    cat schemas/*.py > schemas.py
+    popd
+}
+
+# TODO: We likely need to create a separate job for validating the commited schemas with generated ones
+create_schemas
---
+++ aws-serverless/json2py.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import json
+import sys
+
+def convert(filename_in, filename_out, var_name):
+    with open(filename_in, 'r') as temp:
+        content = json.loads(temp.read())
+
+    with open(filename_out, 'w') as temp:
+        temp.write(f'{var_name} = ')
+        temp.write(json.dumps(content, indent=4, sort_keys=True))
+        temp.write('\n\n')
+
+
+def main():
+    if len(sys.argv) < 4:
+        print("Provide the json filename, outfile name and variable name")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    var_name = sys.argv[3]
+
+    convert(input_file, output_file, var_name)
+
+
+if __name__ == "__main__":
+    main()
