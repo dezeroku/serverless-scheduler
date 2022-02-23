ce8cc62bad334d791eb55d462ae11b36ae5794b4
dezeroku <dezeroku@gmail.com>
2022-02-23 22:54:50 +0100

    Init for schemas

---
+++ aws-serverless/common/schemas.py
@@ -0,0 +1,80 @@
+all_schema = {
+    "oneOf": [
+        {
+            "$ref": "item.json"
+        },
+        {
+            "$ref": "itemWithId.json"
+        }
+    ]
+}
+
+item_schema = {
+    "$schema": "http://json-schema.org/schema#",
+    "description": "Single monitoring job",
+    "properties": {
+        "makeScreenshots": {
+            "type": "boolean"
+        },
+        "ownerId": {
+            "type": "string"
+        },
+        "sleepTime": {
+            "type": "integer"
+        },
+        "url": {
+            "type": "string"
+        }
+    },
+    "required": [
+        "ownerId",
+        "url",
+        "sleepTime",
+        "makeScreenshots"
+    ],
+    "type": "object"
+}
+
+itemwithid_schema = {
+    "$schema": "http://json-schema.org/schema#",
+    "allOf": [
+        {
+            "$schema": "http://json-schema.org/schema#",
+            "description": "Single monitoring job",
+            "properties": {
+                "makeScreenshots": {
+                    "type": "boolean"
+                },
+                "ownerId": {
+                    "type": "string"
+                },
+                "sleepTime": {
+                    "type": "integer"
+                },
+                "url": {
+                    "type": "string"
+                }
+            },
+            "required": [
+                "ownerId",
+                "url",
+                "sleepTime",
+                "makeScreenshots"
+            ],
+            "type": "object"
+        },
+        {
+            "properties": {
+                "id": {
+                    "type": "integer"
+                }
+            },
+            "required": [
+                "id"
+            ],
+            "type": "object"
+        }
+    ],
+    "type": "object"
+}
+
