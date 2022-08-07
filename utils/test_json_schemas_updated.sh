783f101cb7812f65424f62ffc06bd3f66e742a70
dezeroku <dezeroku@gmail.com>
2022-08-07 22:56:05 +0200

    Add a check for common/common/json_schemas.py being up to date with swagger.yaml

---
+++ utils/test_json_schemas_updated.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+RUNDIR="$(readlink -f "$(dirname "$0")")"
+
+SCHEMA_LOCATION="${RUNDIR}/../common/common/json_schemas.py"
+
+"${RUNDIR}"/create_schemas.sh
+echo "pre-commit started"
+pre-commit run --files "${SCHEMA_LOCATION}" > /dev/null || true
+echo "pre-commit finished"
+
+if ! git diff-index --quiet HEAD -- "${SCHEMA_LOCATION}" ; then
+    echo "Python JSON schemas (common/common/json_schemas.py) are not in sync with swagger definitions (swagger/swagger.yaml)!"
+    echo "Run the utils/create_schemas.sh to fix this"
+fi
