902148c73c3f6e9af7599c224f66c832d8196bca
dezeroku <dezeroku@gmail.com>
2022-08-09 02:32:53 +0200

    Add call-level tests for delete

--- items/tests/delete_test.py
+++ items/tests/delete_test.py
@@ -1,7 +1,7 @@
 import pytest
 
 from common import utils
-from items.delete import handler
+from items.delete import delete, handler
 from items.models import MonitorJob, UserData
 from items.schemas import MonitorJobSchema, UserDataSchema
 
@@ -34,6 +34,31 @@ def test_successful_delete(mock_db_table, db_user):
     assert len(loaded.monitors) == 0
 
 
+def test_successful_delete_event(
+    monkeypatch, helpers, table_name, mock_db_table, db_user
+):
+    user_data = mock_db_table.get_item(Key={"id": db_user})["Item"]
+    loaded = UserDataSchema().load(user_data)
+
+    assert len(loaded.monitors) == 1
+
+    item_id = loaded.monitors[0].id
+
+    monkeypatch.setenv("DYNAMO_DB", table_name)
+    event = helpers.EventFactory(
+        cognitoUsername=db_user, pathParameters={"item_id": item_id}
+    )
+    context = None
+    response = delete(event, context)
+
+    assert response["statusCode"] == 200
+
+    user_data = mock_db_table.get_item(Key={"id": db_user})["Item"]
+    loaded = UserDataSchema().load(user_data)
+
+    assert len(loaded.monitors) == 0
+
+
 def test_delete_nonexisting(mock_db_table, db_user):
     user_data = mock_db_table.get_item(Key={"id": db_user})["Item"]
     loaded = UserDataSchema().load(user_data)
@@ -43,6 +68,31 @@ def test_delete_nonexisting(mock_db_table, db_user):
     item_id = loaded.monitors[0].id + 1
 
     response = handler(mock_db_table, db_user, item_id)
+
+    assert response["statusCode"] == 404
+
+    user_data = mock_db_table.get_item(Key={"id": db_user})["Item"]
+    loaded = UserDataSchema().load(user_data)
+
+    assert len(loaded.monitors) == 1
+
+
+def test_delete_nonexisting_event(
+    monkeypatch, helpers, table_name, mock_db_table, db_user
+):
+    user_data = mock_db_table.get_item(Key={"id": db_user})["Item"]
+    loaded = UserDataSchema().load(user_data)
+
+    assert len(loaded.monitors) == 1
+
+    item_id = loaded.monitors[0].id + 1
+
+    monkeypatch.setenv("DYNAMO_DB", table_name)
+    event = helpers.EventFactory(
+        cognitoUsername=db_user, pathParameters={"item_id": item_id}
+    )
+    context = None
+    response = delete(event, context)
 
     assert response["statusCode"] == 404
 
