cfec12617a6e7ed46b65c93f97d8ae61c2066826
dezeroku <dezeroku@gmail.com>
2023-01-07 05:05:14 +0100

    common: hacky way to build common module until github issue is resolved

--- common/bin/package_lambda_runner.sh
+++ common/bin/package_lambda_runner.sh
@@ -4,9 +4,18 @@
 
 set -euo pipefail
 
-DOCKERFILE_DIR="$(readlink -f "$(dirname "$0")")/.."
+DOCKERFILE_DIR="$(readlink -f "$(dirname "$0")/..")"
 
 [ -z "${COMPONENT_NAME}" ] && echo "You have to provide COMPONENT_NAME" && exit 1
 
 rm -rf ./.packaging
-docker buildx build . -f "${DOCKERFILE_DIR}/Dockerfile" --build-arg "COMPONENT_NAME=${COMPONENT_NAME}" --output type=local,dest=./.packaging
+
+# Dirty hack to work around common relative path until https://github.com/python-poetry/poetry/issues/668 gets proper solution
+mkdir -p .packaging/temp
+if [[ ! "$PWD" == "${DOCKERFILE_DIR}" ]]; then
+  cp -r "${DOCKERFILE_DIR}/common" .packaging/temp
+  cp "${DOCKERFILE_DIR}/pyproject.toml" .packaging/temp
+  cp "${DOCKERFILE_DIR}/poetry.lock" .packaging/temp
+fi
+
+docker buildx build . -f "${DOCKERFILE_DIR}/Dockerfile" --build-arg "COMPONENT_NAME=${COMPONENT_NAME}" --output type=local,dest=./.packaging/result
