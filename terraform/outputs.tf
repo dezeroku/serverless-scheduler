cc1c3a7d170fb3305bd563ecab2f71eb8717ce3c
dezeroku <dezeroku@gmail.com>
2023-01-07 10:08:28 +0100

    infra: get rid of baseConfig in favour of tfOutputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,22 @@ output "items_dynamodb_name" {
   value = aws_dynamodb_table.items.name
 }
 
+output "service" {
+  value = var.service
+}
+
+output "stage" {
+  value = var.stage
+}
+
+output "aws_region" {
+  value = var.aws_region
+}
+
+output "base_domain" {
+  value = var.route53_cert_zone
+}
+
 output "api_domain" {
   value = var.api_domain
 }
