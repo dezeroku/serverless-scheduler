2ffe2d84997799b33695e813accbebcfe5841a90
dezeroku <dezeroku@gmail.com>
2023-01-07 09:31:12 +0100

    improve teardown script

--- utils/teardown.sh
+++ utils/teardown.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
-set -e
+set -euo pipefail
+
+# Start in root of the repo
+RUNDIR="$(readlink -f "$(dirname "$0")")"
+cd "${RUNDIR}/.."
+
 
 serverless remove
 
 sls delete_domain
 
 serverless remove-cert
+
+cd terraform
+
+tf destroy
