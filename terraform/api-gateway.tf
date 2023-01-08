e2a313793c0237ceba8a6e4debf2c3ec28c5ab2f
dezeroku <dezeroku@gmail.com>
2023-01-08 17:43:59 +0100

    terraform: auto deploy api-gateway, fixes CORS (and actually deploys stuff)

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -44,6 +44,7 @@ resource "aws_apigatewayv2_api" "api" {
 resource "aws_apigatewayv2_stage" "default" {
   api_id = aws_apigatewayv2_api.api.id
   name   = "$default"
+  auto_deploy = true
 }
 
 resource "aws_apigatewayv2_api_mapping" "example" {
