b3e672169c4a4dcbfee7b2ed5feb4dd972b29764
dezeroku <dezeroku@gmail.com>
2023-01-08 17:52:55 +0100

    fixup teardown

--- utils/teardown.sh
+++ utils/teardown.sh
@@ -7,10 +7,12 @@ RUNDIR="$(readlink -f "$(dirname "$0")")"
 cd "${RUNDIR}/.."
 
 
+# TODO: remove content from front bucket
+
 serverless remove
 
 pushd terraform
-tf destroy
+terraform destroy
 popd
 
 rm -rf .deployment-temp
