a1f25647ab72d03cd3ce8f6a65e9bd4624d1749e
dezeroku <dezeroku@gmail.com>
2023-01-11 03:29:17 +0100

    tf: tf fmt

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -42,8 +42,8 @@ resource "aws_apigatewayv2_api" "api" {
 }
 
 resource "aws_apigatewayv2_stage" "default" {
-  api_id = aws_apigatewayv2_api.api.id
-  name   = "$default"
+  api_id      = aws_apigatewayv2_api.api.id
+  name        = "$default"
   auto_deploy = true
 }
 
