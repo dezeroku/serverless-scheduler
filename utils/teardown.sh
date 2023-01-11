cb3a07419b62e9d5f5dda194d81c4a0de6b6b092
dezeroku <dezeroku@gmail.com>
2023-01-11 05:46:10 +0100

    deploy: move front files upload to TF module

--- utils/teardown.sh
+++ utils/teardown.sh
@@ -9,12 +9,13 @@ cd "${RUNDIR}/.."
 function destroy_terraform_core() {
     pushd terraform/core
 
-    suffix=""
+    suffix="-var-file=../global.tfvars.json"
 
     if [ -f "secret-values.tfvars" ]; then
-        suffix="${suffix}-var-file=secret-values.tfvars"
+        suffix="${suffix} -var-file=secret-values.tfvars"
     fi
 
+    # shellcheck disable=SC2086 # Intended globbing
     terraform destroy ${suffix}
 
     popd
