11526c0c0d940a2c6a386b0c378507e617a2dfd5
dezeroku <dezeroku@gmail.com>
2023-02-02 01:25:06 +0100

    front-upload: set correct MIME type

--- terraform/front/upload-front-bucket.tf
+++ terraform/front/upload-front-bucket.tf
@@ -1,8 +1,16 @@
+# "Smart" MIME thanks to https://engineering.statefarm.com/blog/terraform-s3-upload-with-mime/
+
+locals {
+  mime_types = jsondecode(file("${path.module}/mime.json"))
+}
+
 resource "aws_s3_object" "front_upload" {
   for_each = fileset("${path.module}/../../front/build/", "**")
 
   bucket = var.front_bucket_name
   key    = each.value
   source = "${path.module}/../../front/build/${each.value}"
-  etag   = filemd5("${path.module}/../../front/build/${each.value}")
+
+  etag         = filemd5("${path.module}/../../front/build/${each.value}")
+  content_type = lookup(local.mime_types, replace(regex("\\.[^.]+$", each.value), ".", ""), null)
 }
