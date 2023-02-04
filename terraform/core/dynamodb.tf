e551c0ea12f34ff28ba288e4fb9ee3bcb42e88b3
dezeroku <dezeroku@gmail.com>
2023-02-05 00:02:28 +0100

    init - Rework Items table to contain independent rows

--- terraform/core/dynamodb.tf
+++ terraform/core/dynamodb.tf
@@ -1,10 +1,16 @@
 resource "aws_dynamodb_table" "items" {
   name         = "${local.prefix}-items"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "id"
+  hash_key     = "user_id"
+  range_key    = "job_id"
 
   attribute {
-    name = "id"
+    name = "user_id"
     type = "S"
+  }
+
+  attribute {
+    name = "job_id"
+    type = "N"
   }
 }
