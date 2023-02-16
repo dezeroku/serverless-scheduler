2e97c73cd31456fe8796d80d2c17dede465b8bee
dezeroku <dezeroku@gmail.com>
2023-02-16 02:58:25 +0100

    tf: move dynamodb to its own module

--- terraform/blocks/items-core/dynamodb.tf
+++ terraform/blocks/items-core/dynamodb.tf
@@ -1,16 +1,9 @@
-resource "aws_dynamodb_table" "items" {
-  name         = "${local.prefix}-items"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "user_email"
-  range_key    = "job_id"
-
-  attribute {
-    name = "user_email"
-    type = "S"
+module "dynamodb" {
+  providers = {
+    aws = aws
   }
 
-  attribute {
-    name = "job_id"
-    type = "N"
-  }
+  source = "../../modules/items-dynamodb/"
+
+  table_name = "${local.prefix}-items"
 }
