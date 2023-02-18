c270c480384495885c42ee7549c67755f7500c42
dezeroku <dezeroku@gmail.com>
2023-02-18 23:51:48 +0100

    get rid of serverless framework

--- utils/teardown.sh
+++ utils/teardown.sh
@@ -13,16 +13,7 @@ cd "${RUNDIR}/.."
 [ -z "${DEPLOY_ENV:-}" ] && DEPLOY_ENV="dev"
 
 DESTROY_TARGET="${1}"
-contains "${DEPLOYABLE_TARGETS} API" "${DESTROY_TARGET}" || usage
-
-# TODO: convert this to terraform deployment like the rest
-if [[ "${DESTROY_TARGET}" == "API" ]]; then
-    echo "Starting SLS deployment"
-    pushd serverless
-    DEPLOY_ENV="${DEPLOY_ENV}" serverless remove
-    popd
-    exit 0
-fi
+contains "${DEPLOYABLE_TARGETS}" "${DESTROY_TARGET}" || usage
 
 destroy_terraform "${DESTROY_TARGET}"
 rm -rf ".deployment-temp/${DEPLOY_ENV}/terraform/${DESTROY_TARGET}-outputs.json"
