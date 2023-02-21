c9335b44df638549d63a51e96aa79bebfd1bffe0
dezeroku <dezeroku@gmail.com>
2023-02-21 23:58:30 +0100

    tf: enable streams for items dynamodb

--- terraform/modules/items-dynamodb/dynamodb.tf
+++ terraform/modules/items-dynamodb/dynamodb.tf
@@ -1,8 +1,11 @@
 resource "aws_dynamodb_table" "items" {
-  name         = var.table_name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "user_email"
-  range_key    = "job_id"
+  name             = var.table_name
+  billing_mode     = "PAY_PER_REQUEST"
+  stream_enabled   = true
+  stream_view_type = "NEW_IMAGE"
+
+  hash_key  = "user_email"
+  range_key = "job_id"
 
   attribute {
     name = "user_email"
