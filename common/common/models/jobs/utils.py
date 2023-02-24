1d4a1affd421c3ea0906509160031b8f4b834551
dezeroku <dezeroku@gmail.com>
2023-02-24 03:22:45 +0100

    common: define parse_dict_to_job, which reads dict to a job

---
+++ common/common/models/jobs/utils.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+from common.models.jobs.html_monitor_job import HTMLMonitorJob
+from common.models.jobs.job_type import JobType
+
+
+def map_enum_to_class(entry: Enum):
+    # TODO: similar to JobType, this really looks like it needs some design pattern
+    if entry == JobType.TEST:
+        return ScheduledJob
+
+    if entry == JobType.HTML_MONITOR_JOB:
+        return HTMLMonitorJob
+
+    raise ValueError(f"No matching JobType found for {str(entry)}")
+
+
+def parse_dict_to_job(data: dict):
+    job_type = data.get("job_type")
+    if not job_type:
+        raise ValueError("No job_type provided")
+
+    try:
+        job_type_enum = JobType(job_type)
+    except ValueError as exc:
+        raise ValueError("Incorrect job_type provided") from exc
+
+    job_class = map_enum_to_class(job_type_enum)
+    return job_class(**data)
