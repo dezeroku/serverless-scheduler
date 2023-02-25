a09731e3edbdd7f48888fefe20b7ccd0efceb711
dezeroku <dezeroku@gmail.com>
2023-02-25 22:37:20 +0100

    utils: Add scripts for complete build and deploy

---
+++ utils/complete_deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Start in root of the repo
+RUNDIR="$(readlink -f "$(dirname "$0")")"
+cd "${RUNDIR}"
+
+./deploy.sh items-infra
+./deploy.sh items-lambdas-upload
+./deploy.sh items-front-upload
