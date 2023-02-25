91ac671d9b73fa107c9a02871ce5885dde1bfb01
dezeroku <dezeroku@gmail.com>
2023-02-25 01:01:44 +0100

    items: use scheduler_id

--- common/common/models/__init__.py
+++ common/common/models/__init__.py
@@ -1,5 +1,6 @@
 from common.models.events.scheduler_change_event import SchedulerChangeEvent
 from common.models.events.scheduler_change_type import SchedulerChangeType
+from common.models.jobs.base_job import BaseJob
 from common.models.jobs.html_monitor_job import HTMLMonitorJob
 from common.models.jobs.job_type import JobType
 from common.models.jobs.scheduled_job import ScheduledJob
