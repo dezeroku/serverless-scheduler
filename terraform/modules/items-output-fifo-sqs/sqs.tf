50769fc7b82c585b7f7df2aedc638ff920ac2646
dezeroku <dezeroku@gmail.com>
2023-02-26 23:51:36 +0100

    tf: prepare items-output-fifo-sqs for schedulers

--- terraform/modules/items-output-fifo-sqs/sqs.tf
+++ terraform/modules/items-output-fifo-sqs/sqs.tf
@@ -2,4 +2,13 @@ resource "aws_sqs_queue" "output" {
   name                        = "${var.prefix}-schedule-queue.fifo"
   fifo_queue                  = true
   content_based_deduplication = true
+
+  # We allow Lambda to run for 60 seconds, let's make this x10 to make sure function wasn't throttled
+  visibility_timeout_seconds = 600
+
+  # TODO: handle dead-letter queue
+  #  redrive_policy = jsonencode({
+  #  maxReceiveCount = 10
+  #  deadLetterTargetArn = "some_queue_arn"
+  #  })
 }
