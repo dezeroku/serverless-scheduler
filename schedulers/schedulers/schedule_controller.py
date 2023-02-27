6a5d261b98499b9373754ba8c683bc69d9146a47
dezeroku <dezeroku@gmail.com>
2023-02-27 02:53:31 +0100

    schedulers: init

--- schedulers/schedulers/schedule_controller.py
+++ schedulers/schedulers/schedule_controller.py
@@ -1,2 +1,72 @@
+import json
+import logging
+import os
+
+import boto3
+from mypy_boto3_scheduler import EventBridgeSchedulerClient
+
+from common.models import SchedulerChangeEvent, SchedulerChangeType
+from schedulers.scheduler_manager import SchedulerManager
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
 def schedule_controller(event, context):
-    print("TEST!")
+    # pylint: disable=unused-argument
+    sns_topic_arn = os.environ["DISTRIBUTION_SNS_TOPIC_ARN"]
+    scheduler = boto3.client("scheduler")
+    scheduler_group = os.environ["SCHEDULERS_GROUP"]
+    scheduler_role_arn = os.environ["SCHEDULERS_ROLE_ARN"]
+
+    failed_ids = handler(
+        event["Records"], sns_topic_arn, scheduler, scheduler_group, scheduler_role_arn
+    )
+    return {"batchItemFailures": [{"itemIdentifier": fid} for fid in failed_ids]}
+
+
+def handler(
+    records: list,
+    sns_topic_arn: str,
+    scheduler: EventBridgeSchedulerClient,
+    scheduler_group: str,
+    scheduler_role_arn: str,
+):
+    message_ids = list(map(lambda x: x["messageId"], records))
+    try:
+        for rec in records:
+            change_event = SchedulerChangeEvent(**json.loads(rec["body"]))
+
+            if change_event.change_type == SchedulerChangeType.REMOVE:
+                job = None
+            else:
+                job = change_event.scheduled_job
+
+            manager = SchedulerManager(
+                scheduler, scheduler_group, change_event.scheduler_id, job
+            )
+
+            if change_event.change_type == SchedulerChangeType.CREATE:
+                manager.create(sns_topic_arn, scheduler_role_arn)
+            elif change_event.change_type == SchedulerChangeType.MODIFY:
+                manager.update(sns_topic_arn, scheduler_role_arn)
+            elif change_event.change_type == SchedulerChangeType.REMOVE:
+                manager.delete()
+            else:
+                raise ValueError(
+                    f"Not supported change_type: {change_event.change_type}"
+                )
+
+            message_ids.pop(0)
+    except Exception as exc:
+        # pylint: disable=broad-exception-caught
+        # This is being a little lazy, but it really doesn't matter what goes wrong here
+        logger.error(
+            "Encountered an exception processing %s : %s", rec["messageId"], exc
+        )
+        raise exc
+
+    # Returns a list of messageIDs that couldn't be processed successfully
+    # It stops processing on the first failure and returns the failed + all
+    # unprocessed event IDs (because of the input queue being a FIFO)
+    return message_ids
---
+++ schedulers/schedulers/scheduler_manager.py
@@ -0,0 +1,76 @@
+# TODO: Sadly no tests for this yet
+# as moto doesn't support EventBridge Scheduler at the time this is being written
+
+import math
+
+from mypy_boto3_scheduler import EventBridgeSchedulerClient
+
+from common.models import ScheduledJob
+
+
+class SchedulerManager:
+    """
+    Simple wrapper that controls EventBridge Scheduler state
+    """
+
+    # TODO: do we really case what ScheduledJob is?
+    # This could be made more generic, just getting any str to pass further on
+    # On the other hand, it's always nice to revalidate
+    def __init__(
+        self,
+        client: EventBridgeSchedulerClient,
+        scheduler_group: str,
+        scheduler_id: str,
+        job_config: ScheduledJob = None,
+    ):
+        self.client = client
+        self.scheduler_group = scheduler_group
+        self.scheduler_id = scheduler_id
+        self.job_config = job_config
+
+    def _modify_in_aws(self, method, target_sns_arn, target_sns_role):
+        # Create and update take the same parameters
+        # Update overrides the state with default values if not provided
+        # Thus use a common helper to make sure the params are the same between creation and update
+        method(
+            GroupName=self.scheduler_group,
+            Name=self.scheduler_id,
+            FlexibleTimeWindow={"Mode": "OFF"},
+            ScheduleExpression=SchedulerManager.get_schedule_expression(
+                self.job_config
+            ),
+            Target={
+                "Arn": target_sns_arn,
+                "RoleArn": target_sns_role,
+                "Input": self.job_config.json(),
+            },
+        )
+
+    def create(self, target_sns_arn: str, target_sns_role: str):
+        self._modify_in_aws(
+            self.client.create_schedule, target_sns_arn, target_sns_role
+        )
+
+    def update(self, target_sns_arn: str, target_sns_role: str):
+        # There's no check in place to avoid updates that don't check the state
+        # It's cheaper to just call the update function than add custom check
+        # logic and increase run time
+        self._modify_in_aws(
+            self.client.update_schedule, target_sns_arn, target_sns_role
+        )
+
+    def delete(self):
+        self.client.delete_schedule(
+            GroupName=self.scheduler_group, Name=self.scheduler_id
+        )
+
+    @staticmethod
+    def get_schedule_expression(job_config: ScheduledJob):
+        if (sleep_time := job_config.sleep_time) < 1:
+            raise ValueError("sleep_time must be at least 1 second")
+
+        # Round up to full minutes, as the time provided is in seconds
+        if (sleep_time_minutes := math.ceil(sleep_time / 60)) == 1:
+            return "rate(1minute)"
+
+        return f"rate({sleep_time_minutes}minutes)"
