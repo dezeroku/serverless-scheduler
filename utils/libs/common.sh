18f32bf53b8d537ccfc40b671c04eda74dc635e1
dezeroku <dezeroku@gmail.com>
2023-03-02 02:18:00 +0100

    utils: generalize build.sh

--- utils/libs/common.sh
+++ utils/libs/common.sh
@@ -9,6 +9,11 @@ function contains() {
     [[ " $1 " =~ .*\ $2\ .* ]] && return 0 || return 1
 }
 
+
+PLUGINS_DIR="./plugins"
+AVAILABLE_PLUGINS="$(find "${PLUGINS_DIR}" -mindepth 1 -maxdepth 1 -type d | tr '\n' ' ' | sed 's#./##' | head -c -1)"
+export AVAILABLE_PLUGINS
+
 DEPLOY_DIR="${RUNDIR}/../.deployment-temp/build"
 mkdir -p "${DEPLOY_DIR}"
 DEPLOY_DIR="$(readlink -f "${DEPLOY_DIR}")"
