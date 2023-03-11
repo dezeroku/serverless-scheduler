8df3101994d08adee82cd085dd11b4eb940934e3
dezeroku <dezeroku@gmail.com>
2023-03-11 02:41:19 +0100

    ci: test generations for being up-to-date

--- utils/test_json_schemas_updated.sh
+++ utils/test_json_schemas_updated.sh
@@ -3,13 +3,16 @@ set -e
 
 RUNDIR="$(readlink -f "$(dirname "$0")")"
 
-SCHEMA_LOCATION="${RUNDIR}/../items/items/json_schemas.py"
+SCHEMA_LOCATION="$(readlink -f "${RUNDIR}/../items/items/json_schemas.py")"
+
+pushd "${RUNDIR}/.."
 
 "${RUNDIR}"/create_schemas.sh
 
-if ! git diff-index --quiet HEAD -- "${SCHEMA_LOCATION}" ; then
+if [ -z "$(git status --porcelain -- "${SCHEMA_LOCATION}")" ] ; then
+    echo "Schemas are up-to-date!"
+else
     echo "Python JSON schemas (items/items/json_schemas.py) are not in sync with swagger definitions (items/swagger.yaml)!"
     echo "Run the utils/create_schemas.sh to fix this"
-else
-    echo "Schemas are up-to-date!"
+    exit 1
 fi
